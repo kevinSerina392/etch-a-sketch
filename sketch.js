caa107e9c7384a1e2787eb728a1f4e675c7f55a5
Kevin <kevinserina392@gmail.com>
2023-04-10 19:10:44 -0600

    Added some divs to start

---
+++ sketch.js
@@ -0,0 +1,11 @@
+const grid = document.querySelector('.grid')
+
+
+const box = document.createElement('div');
+
+box.style.color = 'blue';
+box.style.height = '100px';
+box.style.width = '100px';
+
+grid.appendChild(box);
+
